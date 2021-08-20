64d8847068a50573ccdc00104bee94bd18465d06
your_github_username <guptasumit922@gmail.com>
2021-08-20 11:46:48 +0530

    transition to server side

---
+++ Part-2/phonebook-step11-12/src/App.js
@@ -0,0 +1,172 @@
+import React, { useState, useEffect} from "react";
+import axios from 'axios'
+import {retrieve,remove,create,update} from './services/persons'
+
+
+
+
+
+const Notification = ({message}) => {
+  if(message === null)
+    return null
+
+  return (
+      <div className='coloring'>
+        {message}
+      </div>
+  )
+
+
+}
+
+
+
+const Displayer = ({ searchString, persons, searchResult,setPersons }) => {
+
+  function handleDelete(person){
+      let result = window.confirm(`Do you want to delete ${person.name}`)
+      if (result === true) {
+        remove(person.id)
+        retrieve().then( (initialData) => setPersons(initialData)  )
+      }
+  }
+
+
+
+  if (searchString === "") {
+    return (
+      <>
+        <ul>
+          {persons.map((person) => (
+            <li key={person.name}>
+              {" "}
+              {person.name} {person.number}
+              <button onClick={() => handleDelete(person)}> Delete </button>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <ul>
+          {searchResult.map((search) => (
+            <li key={search.name}>
+              {" "}
+              {search.name} {search.number}
+              <button onClick={() => handleDelete(search)}> Delete </button>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+};
+
+
+
+const App = () => {
+    
+    
+    
+    
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNum, setNewNum] = useState("");
+  const [searchString, setSearchString] = useState("");
+  const [searchResult, setSearchResult] = useState([]);
+  const [notif,setNotif] = useState(null)
+  
+  
+  
+  
+  // using the useEffect for fetching the data from server and setting it to the component after first rendering..
+    
+    useEffect( () => {
+        retrieve().then( (initialData) => setPersons(initialData)  )
+      }
+  , [])
+
+    
+    
+
+  function addName(event) {
+    //if trying to add the people in the phonebook
+    if (searchString === "") {
+      event.preventDefault();
+      const nameObject = {
+        name: newName,
+        number: newNum,
+        date: new Date(),
+        id: persons.length + 1
+      };
+
+      console.log("button clicked", event.target);
+      if (persons.find((person) => person.name === nameObject.name) === undefined) {
+        const newData = create(nameObject).then( (person) => setNotif( `${person.name} is added` ) )
+        retrieve().then( (initialData) => setPersons(initialData) )
+        //setPersons(persons.concat(nameObject));
+      }
+      else {
+          let result = window.confirm(`${nameObject.name}is already added to phonebook,replace the old number with new one`)
+          if(result === true) {
+            let oldPerson = persons.find((person) => person.name === nameObject.name)
+            let oldId = oldPerson.id
+            nameObject.id = oldId
+            update(nameObject,oldId)
+            retrieve().then((initialData) => setPersons(initialData))
+          }
+      }
+    }
+
+    //if searching on the phonebook
+    else {
+      event.preventDefault();
+      const searchArray = persons.filter(
+        (person) => person.name.toLowerCase().search(searchString.toLowerCase()) !== -1
+      );
+
+      setSearchResult(searchArray);
+    }
+  }
+
+  function handleNewName(event) {
+    setNewName(event.target.value);
+  }
+
+  function handleNewNum(event) {
+    setNewNum(event.target.value);
+  }
+
+  function handlesetSearchString(event) {
+    setSearchString(event.target.value);
+  }
+
+  return (
+    <div>
+      <Notification message{notif}/>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        filter shown with
+        <input value={searchString} onChange={handlesetSearchString} />
+        <h2>add a new</h2>
+        <div>
+          name: <input value={newName} onChange={handleNewName} />
+        </div>
+        number: <input value={newNum} onChange={handleNewNum} />
+        <br />
+        <button type="submit">add</button>
+      </form>
+
+      <Displayer
+        searchString={searchString}
+        persons={persons}
+        searchResult={searchResult}
+        setPersons = {setPersons}
+      />
+    </div>
+  );
+};
+
+export default App;
---
+++ Part-2/phonebook-step11-12/src/services/persons.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+const baseURL = 'http://localhost:3002/persons'
+
+
+
+
+
+   function create(newObject) {
+       const request = axios.post(baseURL, newObject)
+       return request.then((response) => response.data)
+   }
+
+
+ function retrieve(){
+        const request = axios.get(baseURL)
+        return request.then (  (response)  => response.data)
+}
+
+
+ function remove (id) {
+    const promise = axios.delete(`${baseURL}/${id}`)
+}
+
+
+    function update(newObject,id){
+        const request = axios.put( `${baseURL}/${id}`,newObject)
+        return request.then( response => response.data   )
+    }
+
+
+
+export {
+    create,
+    retrieve,
+    remove,
+    update
+}

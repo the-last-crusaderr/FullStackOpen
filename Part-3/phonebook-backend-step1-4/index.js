45c80957094fff887d7dd62e328efd58c23b74cf
your_github_username <guptasumit922@gmail.com>
2021-08-23 16:36:40 +0530

    working on server side

---
+++ Part-3/phonebook-backend-step1-4/index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+
+const app = express()
+
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/api/persons',(request,response) => {
+	response.json(persons)
+} )
+
+
+
+app.get('/api/persons/:id', (request,response) => {
+	const id = Number(request.params.id)
+	const person = persons.find( person => person.id === id )
+	if(person)
+		response.send(person)
+	else
+		response.status(404).end()
+} )
+
+app.get('/info',(request,response)=> {
+	const date = Date()
+	response.send(`<p>Phonebook has info for ${persons.length} people</p>
+		<p>${date} </p>`
+	)
+
+})
+
+
+app.delete('/api/persons/:id' ,(request,response) => {
+	const id = Number(request.params.id)
+	persons = persons.filter( person => person.id !== id )
+	response.status(204).end()
+}  )
+
+
+const PORT = 3001
+app.listen( PORT, () => console.log(`Listening to port-${PORT}`) )
+
+
